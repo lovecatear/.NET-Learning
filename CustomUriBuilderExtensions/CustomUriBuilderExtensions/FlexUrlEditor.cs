58f125999e68028f16203f11bf07627dca975b4a
Jay <saber83528+github@gmail.com>
2023-09-23 11:18:30 +0800

    Add files via upload
    整理 網址、路徑

---
+++ CustomUriBuilderExtensions/CustomUriBuilderExtensions/FlexUrlEditor.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Specialized;
+using System.Text;
+using System.Web;
+
+namespace CustomUriBuilderExtensions;
+
+// 自定義的 UriBuilder 擴充類別
+public class FlexUrlEditor : UriBuilder
+{
+    private readonly NameValueCollection collection; // 存儲查詢參數的集合
+
+    /// <summary>
+    /// 初始化 FlexUrlEditor 物件並解析 URL 中的查詢參數。
+    /// </summary>
+    /// <param name="url">要操作的 URL 字串。</param>
+    public FlexUrlEditor(string url) : base(url)
+    {
+        // 使用 HttpUtility.ParseQueryString 方法解析 URL 中的查詢參數
+        // 並將它們存儲在 collection 變數中
+        collection = HttpUtility.ParseQueryString(new Uri(url).Query, Encoding.UTF8);
+    }
+
+    /// <summary>
+    /// 存取查詢參數的值，並允許設定新的值。
+    /// </summary>
+    /// <param name="key">查詢參數的名稱。</param>
+    /// <returns>查詢參數的值。</returns>
+    public string this[string key]
+    {
+        get { return collection[key]; }
+        set
+        {
+            if (value == null && collection[key] != null)
+            {
+                // 如果新值為 null 且存在相應的查詢參數，則刪除它
+                collection.Remove(key);
+            }
+            else
+            {
+                // 否則，設定查詢參數的值
+                collection[key] = value;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 生成編輯後的 URL 字串。
+    /// </summary>
+    /// <returns>編輯後的 URL 字串。</returns>
+    public string GenUrl()
+    {
+        // 編碼並解碼查詢參數，然後設定為 UriBuilder 的查詢部分 REF: https://goo.gl/gHmGUz
+        base.Query = Uri.EscapeUriString(HttpUtility.UrlDecode(collection.ToString()));
+
+        // 如果 URL 使用默認的 HTTP（80）或 HTTPS（443）端口，則移除端口號
+        if (base.Uri.IsDefaultPort) base.Port = -1;
+
+        // 返回最終的 URL 字串
+        return base.ToString();
+    }
+}
---
+++ CustomUriBuilderExtensions/CustomUriBuilderExtensions/RouteController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomUriBuilderExtensions;
+
+/// <summary>
+/// 用於處理 URL 操作的控制器
+/// </summary>
+[Route("api/[controller]")]
+[ApiController]
+public class RouteController : ControllerBase
+{
+    /// <summary>
+    /// HTTP GET 請求，用於獲取編輯後的 URL
+    /// </summary>
+    /// <returns>包含生成的 URL 的 JSON 响應</returns>
+    [HttpGet]
+    public IActionResult Get()
+    {
+        // 使用 HttpContext.Request.GetDisplayUrl() 獲取目前請求的 URL
+        var builder = new FlexUrlEditor(HttpContext.Request.GetDisplayUrl());
+        var builder1 = builder.GenUrl();
+
+        // 以下範例演示如何使用 FlexUrlEditor 來修改和刪除查詢參數
+        var url = "http://blog.darkthread.net/?a=1&b=2&c=3";
+        builder = new FlexUrlEditor(url);
+        builder["b"] = null; // 設定查詢參數 "b" 的值為 null，這將導致它被刪除
+        builder["c"] = "中文"; // 設定查詢參數 "c" 的值為 "中文"，這將對值進行編碼
+        var builder2 = builder.GenUrl();
+
+        // 創建一個新的 FlexUrlEditor，並設定 URL 的協定為 "http"，端口為 8888
+        builder = new FlexUrlEditor("https://blog.darkthread.net")
+        {
+            Scheme = "http",
+            Port = 8888
+        };
+        var builder3 = builder.GenUrl();
+
+        // 返回包含生成的 URL 的 JSON 响應
+        return StatusCode(StatusCodes.Status200OK, new
+        {
+            builder1,
+            builder2,
+            builder3
+        });
+    }
+}
